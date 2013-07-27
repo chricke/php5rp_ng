98ebb33f49c656cdbcc6da97d1b32daf7a0c1816
Dr. Kibitz <info@drkibitz.com>
2013-07-27 00:29:45 -0700

    Added optional $base_uri argument, removed ending ?>, whitespace fixes

--- ProxyHandler.class.php
+++ ProxyHandler.class.php
@@ -12,14 +12,22 @@ class ProxyHandler
     private $pragma=false;
     private $client_headers=array();
 
-    function __construct($url, $proxy_url) {
+    function __construct($url, $proxy_url, $base_uri = null) {
         // Strip the trailing '/' from the URLs so they are the same.
         $this->url = rtrim($url,'/');
         $this->proxy_url =  rtrim($proxy_url,'/');
 
+        if ($base_uri === null && isset($_SERVER['REDIRECT_URL'])) {
+            $base_uri = dirname($_SERVER['REDIRECT_URL']);
+        }
+
         // Parse all the parameters for the URL
-        if (isset($_SERVER["REQUEST_URI"])) {
-            $proxy_url .= $_SERVER["REQUEST_URI"];
+        if (isset($_SERVER['REQUEST_URI'])) {
+            $request_uri = $_SERVER['REQUEST_URI'];
+            if ($base_uri && strpos($request_uri, $base_uri) === 0) {
+                $request_uri = substr($request_uri, strlen($base_uri));
+            }
+            $proxy_url .= $request_uri;
         }
         else {
             // Add the '/' at the end
@@ -50,11 +58,11 @@ class ProxyHandler
             $this->setCurlOption(CURLOPT_POST, true);
 
             // Encode and form the post data
-		   if (!isset($HTTP_RAW_POST_DATA)){
-			$HTTP_RAW_POST_DATA = file_get_contents("php://input"); 
-		   }
-	
-		   $this->setCurlOption(CURLOPT_POSTFIELDS, $HTTP_RAW_POST_DATA);
+           if (!isset($HTTP_RAW_POST_DATA)){
+            $HTTP_RAW_POST_DATA = file_get_contents("php://input");
+           }
+
+           $this->setCurlOption(CURLOPT_POSTFIELDS, $HTTP_RAW_POST_DATA);
 
             unset($post);
         }
@@ -62,7 +70,7 @@ class ProxyHandler
             // Set the request method
             $this->setCurlOption(CURLOPT_CUSTOMREQUEST, $_SERVER['REQUEST_METHOD']);
         }
-      	elseif ($_SERVER['REQUEST_METHOD'] == 'PUT'){
+        elseif ($_SERVER['REQUEST_METHOD'] == 'PUT'){
             // Set the request method.
             $this->setCurlOption(CURLOPT_UPLOAD, 1);
 
@@ -115,7 +123,7 @@ class ProxyHandler
         }
         return $length;
     }
-    
+
     protected function handleClientHeaders() {
         $headers = $this->request_headers();
 
@@ -127,7 +135,7 @@ class ProxyHandler
                     $this->setClientHeader(sprintf('%s: %s', $header, $value));
                     break;
             }
-        } 
+        }
     }
 
     protected function readResponse(&$cu, $string) {
@@ -149,25 +157,23 @@ class ProxyHandler
         return $length;
     }
 
-	function request_headers(){
-	    if(function_exists("apache_request_headers")){ // If apache_request_headers() exists
-			if($headers = apache_request_headers()){ // And works...
+    function request_headers(){
+        if(function_exists("apache_request_headers")){ // If apache_request_headers() exists
+            if($headers = apache_request_headers()){ // And works...
 
-			    return $headers; // Use it
-			}
-	    }
+                return $headers; // Use it
+            }
+        }
 
-	    $headers = array();
+        $headers = array();
 
-	    foreach(array_keys($_SERVER) as $skey){
-			if(substr($skey, 0, 5) == "HTTP_"){
-		    	$headername = str_replace(" ", "-", ucwords(strtolower(str_replace("_", "", substr($skey, 0, 5)))));
-		    	$headers[$headername] = $_SERVER[$skey];
-			}
-	    }
+        foreach(array_keys($_SERVER) as $skey){
+            if(substr($skey, 0, 5) == "HTTP_"){
+                $headername = str_replace(" ", "-", ucwords(strtolower(str_replace("_", "", substr($skey, 0, 5)))));
+                $headers[$headername] = $_SERVER[$skey];
+            }
+        }
 
-	    return $headers;
-	}
+        return $headers;
+    }
 }
-
-?>
