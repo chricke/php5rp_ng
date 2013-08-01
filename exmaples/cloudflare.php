0bbabc887bbe836b1ee7d2e3b375b16da02498e1
Dr. Kibitz <info@drkibitz.com>
2013-08-01 02:46:06 -0700

    Added more examples

--- exmaples/cloudflare.php
+++ exmaples/cloudflare.php
@@ -4,16 +4,21 @@ if (!@include __DIR__ . '/../ProxyHandler.class.php') {
     die('Could not load proxy');
 }
 
+/**
+ * CloudFlare doesn't allow a request to certain IPs
+ * Namely the same IP the request is coming from.
+ * Stripping CloudFlare headers allows this.
+ */
 class CloudFlareSafeProxy extends ProxyHandler
 {
-    public function setClientHeader($header) {
-        if (substr($header, 0, 3) !== 'Cf-') {
-            parent::setClientHeader($header);
+    public function setClientHeader($headerName, $value) {
+        if (substr($headerName, 0, 3) !== 'Cf-') {
+            parent::setClientHeader($headerName, $value);
         }
     }
 }
 
-$proxy = new CloudFlareSafeProxy('http://internal.example.com');
+$proxy = new CloudFlareSafeProxy('http://internal.example.org');
 $proxy->execute();
 //print_r($proxy->getCurlInfo()); // Uncomment to see request info
 $proxy->close();
---
+++ exmaples/https.php
@@ -0,0 +1,22 @@
+<?php
+
+if (!@include __DIR__ . '/../ProxyHandler.class.php') {
+    die('Could not load proxy');
+}
+
+$proxy = new ProxyHandler('https://raw.github.com/chricke/php5rp_ng/master/README.md');
+
+// Prevents cURL from hanging on errors
+$proxy->setCurlOption(CURLOPT_CONNECTTIMEOUT, 1);
+$proxy->setCurlOption(CURLOPT_TIMEOUT, 5);
+
+// This ignores HTTPS certificate verification, libcurl decided not to bundle ca certs anymore.
+// Alternatively, specify CURLOPT_CAINFO, or CURLOPT_CAPATH if you have access to your cert(s)
+$proxy->setCurlOption(CURLOPT_SSL_VERIFYPEER, false);
+
+// Check for a success
+if ($proxy->execute() === false) {
+    echo $proxy->getCurlError();
+}
+
+$proxy->close();
